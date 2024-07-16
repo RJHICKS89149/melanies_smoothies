bc4453eae3daf0dd8ffba8e87bb460c4cad28661
RJHICKS89149 <richard.hicks@creditone.com>
2024-07-16 11:38:54 -0700

    debugging

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,13 +10,13 @@ st.write("""
 Choose the fruits you want in your custom Smoothie!
 """)
 
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 
-cnx = st.connection("snowflake")
-session = cnx.session()
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
