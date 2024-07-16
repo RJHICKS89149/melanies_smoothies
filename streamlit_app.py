9093cfcabbe6dd6961ee26ebfb5311f271cbfafe
RJHICKS89149 <richard.hicks@creditone.com>
2024-07-16 11:37:20 -0700

    debugging

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-import pandas as pd
+# import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw: ")
@@ -15,8 +15,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 
-# cnx = st.connection("snowflake")
-# session = cnx.session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
