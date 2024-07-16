07f69994fd4a63482dc6e552e87c8175cc18bc35
RJHICKS89149 <richard.hicks@creditone.com>
2024-07-16 11:20:38 -0700

    import Pandas as pd

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-import Pandas
+import Pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw: ")
@@ -39,7 +39,7 @@ if ingredients_list:
 
         search_on=pd_df,loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search valude for ', fruit_chosen, ' is', search_on, '.')
-        
+
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
